9312b992e1b802ceb077412bd0cdef13d1b14166
vikingslord <vikingslord98@gmail.com>
2023-08-22 16:33:12 +0600

    added requesthelpers for automapper

---
+++ AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            //source //destination //adding item properties to auction dto
+            CreateMap<Auction, AuctionDto>().IncludeMembers(member => member.Item);
+
+            //source //destination 
+            CreateMap<Item, AuctionDto>();
+
+            CreateMap<CreateAuctionDto, Auction>().ForMember(destination => destination.Item, o => o.MapFrom(s => s));
+
+            CreateMap<AuctionDto, Item>();
+        }
+    }
+}
