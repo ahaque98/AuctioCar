543d9ebafe122b017f20f52993e618fb1528f0c3
vikingslord <vikingslord98@gmail.com>
2023-08-22 17:52:39 +0600

    added GET mathod

--- AuctionService/Controllers/AuctionsController.cs
+++ AuctionService/Controllers/AuctionsController.cs
@@ -1,11 +1,39 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuctionService.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/auctions")]
     [ApiController]
     public class AuctionsController : ControllerBase
     {
+        private readonly AuctionDbContext _context;
+        private readonly IMapper _mapper;
+        public AuctionsController(AuctionDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AuctionDto>>> Get()
+        {
+            var auctions = await _context.Auctions
+                .Include(x => x.Item)
+                .OrderBy(x => x.Item.Make)
+                .ToListAsync();
+
+            if (auctions is null)
+            {
+                return NotFound();
+            }
+
+            var auctionsResult = _mapper.Map<AuctionDto>(auctions);
+
+            return Ok(auctionsResult);
+        }
     }
 }
