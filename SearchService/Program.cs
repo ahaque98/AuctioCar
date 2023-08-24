1f976467c478e1643d92b528d7217b8805ded4db
vikingslord <vikingslord98@gmail.com>
2023-08-24 15:26:38 +0600

    added mongoDB configuration

--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,25 +1,34 @@
-namespace SearchService
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-
-            app.UseAuthorization();
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
 
 
-            app.MapControllers();
+var builder = WebApplication.CreateBuilder(args);
 
-            app.Run();
-        }
-    }
-}
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+//mongoDB connection
+
+await DB.InitAsync("SearchDb", MongoClientSettings
+    .FromConnectionString(
+    builder.Configuration.GetConnectionString("MongoDbConnection")
+    ));
+
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
+
+app.Run();
